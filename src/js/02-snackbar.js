e2efd88f7585baf66900d26ca6d85970895482e8
Dorota Dobija <117933735+DorotaDobija@users.noreply.github.com>
2024-07-09 12:57:59 +0200

    code refractoring task 2

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -3,33 +3,40 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const formEl = document.querySelector("form")
 
-const makePromise = (event) => {
-event.preventDefault();
-const delayValue = event.target.elements.delay.value;
-const promiseStatute = event.target.elements.state.value;
-
-const promise = new Promise((resolve, rejected) => {
-    setTimeout(() => {
-        if (promiseStatute === "fulfilled") {
-        resolve(delayValue);
+const generatePromise = (delay, state) => new Promise((resolve, rejected) => {
+setTimeout(() => {
+    if (state === "fulfilled") {
+        resolve(delay);
     } else {
-        rejected(delayValue)
-     }}
-    , delayValue)
-    
+        rejected(delay)
+    }
+    }
+    , delay)
 });
+    
+const handlePromiseResult = (event) => {
+    event.preventDefault();
+    const delayValue = event.target.elements.delay.value;
+    const promiseStatute = event.target.elements.state.value;
+
+    const promise = generatePromise(delayValue, promiseStatute);
+
     promise.then(delay => iziToast.success({
         title: '',
         message: `Fulfilled promise in ${delay}ms`,
         position: 'topRight',
     }))
         .catch(delay => iziToast.error({
-        title: '',
-        message: `Rejected promise in ${delay}ms`,
-        position: 'topRight',
-        }))
-    formEl.reset();
+            title: '',
+            message: `Rejected promise in ${delay}ms`,
+            position: 'topRight',
+        }));
+    
+    clearForm(formEl);
 }
 
+const clearForm = (form) => {
+    form.reset()
+}
 
-formEl.addEventListener("submit", makePromise);
+formEl.addEventListener("submit", handlePromiseResult);
